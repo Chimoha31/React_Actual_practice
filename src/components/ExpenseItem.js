9c7c29679a3fa035d6529c7f19c48ba281e057ae
Chimoha31 <mokochii1108@gmail.com>
2021-09-24 15:52:09 -0700

    create_ExpenseItem.css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
   return (
-   <h2>Expensive Item!</h2>
+    <div>
+      <div className="expense-item">March 28th 2021</div>
+      <div className="expense-otem__description">
+        <h2>Car Insurance</h2>
+      </div>
+      <div className="expense-item__price">＄294.67</div>
+    </div>
   );
-};
+}
 
 export default ExpenseItem;
